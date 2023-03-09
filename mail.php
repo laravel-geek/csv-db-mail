e50d8f087021e3c4aa6fe5d53bc0e5f9900909c4
Nikita P <81123005+laravel-geek@users.noreply.github.com>
2023-03-09 13:20:06 +0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -12,27 +12,20 @@ try {
 
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $stmt = $pdo->prepare("SELECT * FROM users");
-    $stmt->execute();
-    $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
     $sql = "INSERT INTO mailings (name, text, user_id) VALUES (:name, :text, :user_id)";
     $stmt = $pdo->prepare($sql);
 
-    foreach ($users as $user) {
-        // Проверка, была ли рассылка уже отправлена данному пользователю
-        $stmt_check = $pdo->prepare("SELECT * FROM mailings WHERE user_id = :user_id AND sent = 1");
-        $stmt_check->bindParam(':user_id', $user['id']);
-        $stmt_check->execute();
+    $stmt_users = $pdo->prepare("SELECT u.* FROM users u LEFT JOIN mailings m ON u.id = m.user_id AND m.sent = 1 WHERE m.id IS NULL");
+    $stmt_users->execute();
+    $users = $stmt_users->fetchAll(PDO::FETCH_ASSOC);
 
-        if ($stmt_check->rowCount() == 0) { // Если рассылка не была отправлена, добавляем запись в таблицу рассылок
-            $name = 'Название рассылки';
-            $text = 'Текст рассылки';
-            $stmt->bindParam(':name', $name);
-            $stmt->bindParam(':text', $text);
-            $stmt->bindParam(':user_id', $user['id']);
-            $stmt->execute();
-        }
+    foreach ($users as $user) {
+        $name = 'Название рассылки';
+        $text = 'Текст рассылки';
+        $stmt->bindParam(':name', $name);
+        $stmt->bindParam(':text', $text);
+        $stmt->bindParam(':user_id', $user['id']);
+        $stmt->execute();
     }
 
 } catch(PDOException $e) {
