48e945a83aece8ed25e4d15afe165ca34dcd597d
Nikita P <81123005+laravel-geek@users.noreply.github.com>
2023-03-09 13:14:44 +0300

    Delete users.sql

--- users.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE users (
-                       id INT AUTO_INCREMENT PRIMARY KEY,
-                       number VARCHAR(255) NOT NULL,
-                       name VARCHAR(255) NOT NULL
-);
