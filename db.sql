24a35637ee05e71497ff1fac9293860392578cb1
Nikita P <81123005+laravel-geek@users.noreply.github.com>
2023-03-09 13:14:12 +0300

    Update and rename mailings.sql to db.sql

--- db.sql
+++ db.sql
@@ -5,4 +5,10 @@ CREATE TABLE mailings (
                           user_id INT NOT NULL,
                           sent BOOLEAN NOT NULL DEFAULT 0,
                           FOREIGN KEY (user_id) REFERENCES users(id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) 
+
+CREATE TABLE users (
+                       id INT AUTO_INCREMENT PRIMARY KEY,
+                       number VARCHAR(255) NOT NULL,
+                       name VARCHAR(255) NOT NULL
+);
