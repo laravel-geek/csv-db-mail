c32300232dcc2ae9da118ef178dcefa44f6d79f3
Nikita P <81123005+laravel-geek@users.noreply.github.com>
2023-03-09 13:31:49 +0300

    Update db.sql

--- db.sql
+++ db.sql
@@ -10,5 +10,6 @@ CREATE TABLE mailings (
 CREATE TABLE users (
                        id INT AUTO_INCREMENT PRIMARY KEY,
                        number VARCHAR(255) NOT NULL,
-                       name VARCHAR(255) NOT NULL
+                       name VARCHAR(255) NOT NULL,
+                       email VARCHAR(255) NOT NULL
 );
